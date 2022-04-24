55f006934fc82da19856215fbf0bfc9bc54defa5
Piyumika kavindi <piyumikaalmeda@gmail.com>
2022-04-24 20:40:41 +0530

    updated

--- src/main/java/org/restapi/crud/crud/resource/crudresource.java
+++ src/main/java/org/restapi/crud/crud/resource/crudresource.java
@@ -1,11 +1,17 @@
 package org.restapi.crud.crud.resource;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import org.restapi.crud.crud.model.crudmodel;
 import org.restapi.crud.crud.service.crudservice;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/crud")
@@ -18,6 +24,34 @@ public class crudresource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public crudmodel addUser(crudmodel user) {
 		return service.insertUser(user);
+		
+	}
+	
+
+	
+	@Path("/retrieve")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<crudmodel> getUser() throws SQLException{
+		return service.getUser();
+			
+	}
+	
+	
+	@Path("/retrieveById/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<crudmodel> getUser(@PathParam("id") int id) throws SQLException{
+		return service.getUserById(id);
+			
+	}
+	
+	
+	@Path("/updateUser")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public crudmodel updateUser(crudmodel user) {
+		return service.updatetUser(user);
 		
 	}
 }
--- src/main/java/org/restapi/crud/crud/service/crudservice.java
+++ src/main/java/org/restapi/crud/crud/service/crudservice.java
@@ -3,7 +3,9 @@ package org.restapi.crud.crud.service;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.restapi.crud.crud.model.crudmodel;
 
@@ -33,6 +35,62 @@ public class crudservice {
 			ps.setLong(2, user.getAge());
 			
 			ps.execute();
+		}catch(Exception e) {
+			System.out.println(e +"data insert unsuccess");
+		}
+		
+		return user;
+	}
+	
+	public ArrayList<crudmodel> getUser() throws SQLException{
+		
+		ArrayList<crudmodel> data = new ArrayList<crudmodel>();		
+		String select = "select * from person";
+		PreparedStatement ps = con.prepareStatement(select);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			 crudmodel model = new crudmodel();
+			 
+			 model.setName(rs.getString("name"));  
+			 model.setAge(rs.getInt("age"));
+			 
+			 data.add(model);
+		}
+		return data;
+	
+	}
+	
+	
+	
+	public ArrayList<crudmodel> getUserById(int id) throws SQLException{
+		
+		ArrayList<crudmodel> data = new ArrayList<crudmodel>();		
+		String select = "select * from person where id =?";
+		PreparedStatement ps = con.prepareStatement(select);
+		ps.setInt(1,id);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			 crudmodel model = new crudmodel();
+			 
+			 model.setName(rs.getString("name"));  
+			 model.setAge(rs.getInt("age"));
+			 data.add(model);
+		}
+		return data;
+	}
+	
+	public crudmodel updatetUser(crudmodel user) {
+		String insert = "update person set name = ? , age =? where id = ?";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(insert);
+		
+			ps.setString(2, user.getName());
+			ps.setInt(3, user.getId());
+			
+			ps.executeUpdate();
 		}catch(Exception e) {
 			System.out.println(e +"data insert unsuccess");
 		}
