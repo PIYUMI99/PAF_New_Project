b01c7fac09097e797edbe87e757f859334a79535
Piyumika kavindi <piyumikaalmeda@gmail.com>
2022-04-25 00:56:43 +0530

    Created user insertion process

--- src/main/java/org/restapi/crud/crud/model/crudmodel.java
+++ src/main/java/org/restapi/crud/crud/model/crudmodel.java
@@ -1,14 +1,27 @@
 package org.restapi.crud.crud.model;
 
 public class crudmodel {
-	
 	private int id;
 	private String name;
-	private int age;
+	private String nic;
+	private String address;
+	private int telNo;
+	private int accNo;
 	
-	
+
 	public crudmodel() {
-		
+
+	}
+
+
+	public crudmodel(int id, String name, String nic, String address, int telNo, int accNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.nic = nic;
+		this.address = address;
+		this.telNo = telNo;
+		this.accNo = accNo;
 	}
 
 
@@ -32,18 +45,47 @@ public class crudmodel {
 	}
 
 
-	public int getAge() {
-		return age;
+	public String getNic() {
+		return nic;
 	}
 
 
-	public void setAge(int age) {
-		this.age = age;
+	public void setNic(String nic) {
+		this.nic = nic;
 	}
 
 
-	
+	public String getAddress() {
+		return address;
+	}
 
-	
 
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public int getTelNo() {
+		return telNo;
+	}
+
+
+	public void setTelNo(int telNo) {
+		this.telNo = telNo;
+	}
+
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+	
+	
+	
 }
+
+	
--- src/main/java/org/restapi/crud/crud/resource/crudresource.java
+++ src/main/java/org/restapi/crud/crud/resource/crudresource.java
@@ -2,11 +2,13 @@ package org.restapi.crud.crud.resource;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.restapi.crud.crud.model.crudmodel;
 import org.restapi.crud.crud.service.crudservice;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -23,35 +25,44 @@ public class crudresource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public crudmodel addUser(crudmodel user) {
-		return service.insertUser(user);
+		 return service.insertUser(user);
+		
+	}
+	
+	@Path("/retrieve")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<crudmodel>  getUser() throws SQLException {
+		 return service.getUser();
+		
+	}
+	
+	@Path("/retrieveById/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<crudmodel>  getUser(@PathParam("id") int id) throws SQLException {
+		return service.getUserById(id);
 		
 	}
 	
 
 	
-	@Path("/retrieve")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<crudmodel> getUser() throws SQLException{
-		return service.getUser();
-			
-	}
-	
-	
-	@Path("/retrieveById/{id}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<crudmodel> getUser(@PathParam("id") int id) throws SQLException{
-		return service.getUserById(id);
-			
-	}
-	
-	
 	@Path("/updateUser")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public crudmodel updateUser(crudmodel user) {
-		return service.updatetUser(user);
+		 return service.updatetUser(user);
 		
 	}
+	
+	@Path("/deleteUserById/{id}")
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public int deleteUser(@PathParam("id") int id) {
+		return service.deletetUser(id);
+		
+	}
+
+	
+	
 }
